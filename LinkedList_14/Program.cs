c07fbe3bd40856c6cb5bcb14c93dd8dcec30b80a
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-18 22:13:10 +0530

    Added Simple LinkedList Program

--- LinkedList_14/Program.cs
+++ LinkedList_14/Program.cs
@@ -5,6 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LinkedList Program");
+            //UC_1
+            SimpleLinkedListUC_1 simple = new SimpleLinkedListUC_1();
+            simple.Add(56);
+            simple.Add(30);
+            simple.Add(70);
         }
     }
 }
